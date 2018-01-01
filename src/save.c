9c2963e72c258e4a8831582e876de71c79750ac6
driedfruit <driedfruit@mindloop.net>
2018-01-01 12:28:36 +0400

    Add Amiga "compatibility" to savefile reader/writer.

--- src/save.c
+++ src/save.c
@@ -24,7 +24,9 @@
 
 #define RPOS(STR) printf("%s Pos: %06x\n", STR, p - &buf[0])
 
+KBgame* KB_loadDAT_amiga(const char* filename);
 KBgame* KB_loadDAT(const char* filename) {
+	return KB_loadDAT_amiga(filename);
 
 	char buf[DAT_SIZE];
 	KBgame *game;
@@ -202,7 +204,7 @@ KBgame* KB_loadDAT(const char* filename) {
 	}
 
 	/* Read hostile foes' troops */
-	for (n = 0; n < MAX_CONTINENTS; n++) {	
+	for (n = 0; n < MAX_CONTINENTS; n++) {
 		for (k = FRIENDLY_FOES; k < MAX_FOES; k++) {
 			game->foe_troops[n][k][0] = READ_BYTE(p);
 			game->foe_troops[n][k][1] = READ_BYTE(p);
@@ -211,7 +213,7 @@ KBgame* KB_loadDAT(const char* filename) {
 	}
 
 	/* Read foe numbers */
-	for (n = 0; n < MAX_CONTINENTS; n++) {	
+	for (n = 0; n < MAX_CONTINENTS; n++) {
 		for (k = FRIENDLY_FOES; k < MAX_FOES; k++) {
 			game->foe_numbers[n][k][0] = READ_BYTE(p);
 			game->foe_numbers[n][k][1] = READ_BYTE(p);
@@ -220,11 +222,11 @@ KBgame* KB_loadDAT(const char* filename) {
 	}
 
 	/* Read dwelling troop type and population count */
-	for (n = 0; n < MAX_CONTINENTS; n++)	
+	for (n = 0; n < MAX_CONTINENTS; n++)
 		for (k = 0; k < MAX_DWELLINGS; k++)
 			game->dwelling_troop[n][k] = READ_BYTE(p);	
 
-	for (n = 0; n < MAX_CONTINENTS; n++)	
+	for (n = 0; n < MAX_CONTINENTS; n++)
 		for (k = 0; k < MAX_DWELLINGS; k++)
 			game->dwelling_population[n][k] = READ_BYTE(p);
 
@@ -273,6 +275,267 @@ KBgame* KB_loadDAT(const char* filename) {
 
 	return game;
 }
+
+
+KBgame* KB_loadDAT_amiga(const char* filename) {
+
+	char buf[DAT_SIZE];
+	KBgame *game;
+	FILE *f;
+	int n, k, map_size;
+	int x, y;
+
+	f = fopen(filename, "rb");
+	if (f == NULL) return NULL;
+
+	n = fread(buf, sizeof(char), DAT_SIZE, f);
+	fclose(f);
+	if (n != DAT_SIZE) return NULL;
+
+	game = malloc(sizeof(KBgame));
+	if (game == NULL) return NULL;
+
+	/* Read out the save from "buf" */
+	char *p = &buf[0];
+
+	/* Player name */
+	memcpy(game->name, p, 10);
+	for (n = 9; n > 0; n--) {
+		if (game->name[n] != ' ') break;
+		game->name[n] = '\0';
+	}
+	p += 11;
+
+	/* Main stats */
+	game->class = READ_BYTE(p);
+	game->rank = READ_BYTE(p);
+	game->spell_power = READ_BYTE(p);
+	game->max_spells = READ_BYTE(p);
+
+	/* Quest progress */
+	for (n = 0; n < MAX_VILLAINS; n++)
+		game->villain_caught[n] = READ_BYTE(p);
+
+	for (n = 0; n < MAX_ARTIFACTS; n++)
+		game->artifact_found[n] = READ_BYTE(p);
+
+	for (n = 0; n < MAX_CONTINENTS; n++)
+		game->continent_found[n] = READ_BYTE(p);
+
+	for (n = 0; n < MAX_CONTINENTS; n++)
+		game->orb_found[n] = READ_BYTE(p);
+
+	/* Spell book */
+	for (n = 0; n < MAX_SPELLS; n++)
+		game->spells[n] = READ_BYTE(p);
+
+	/* More stats */
+	game->knows_magic = READ_BYTE(p);
+	game->siege_weapons = READ_BYTE(p);
+	game->contract = READ_BYTE(p);
+
+	for (n = 0; n < 5; n++)
+		game->player_troops[n] = READ_BYTE(p);
+
+	/* Options and difficulty setting */
+	game->options[0] = READ_BYTE(p);//delay
+
+	game->difficulty = READ_BYTE(p);
+
+	game->options[1] = READ_BYTE(p);//sounds
+	game->options[2] = READ_BYTE(p);//walk beep
+	game->options[3] = READ_BYTE(p);//animation
+	game->options[4] = READ_BYTE(p);//show army size
+
+	/* Player location */
+	game->continent = READ_BYTE(p);
+	game->x = READ_BYTE(p);
+	game->y = READ_BYTE(p);
+	game->last_x = READ_BYTE(p);
+	game->last_y = READ_BYTE(p);
+	game->boat_x = READ_BYTE(p);
+	game->boat_y = READ_BYTE(p);
+	game->boat = READ_BYTE(p);
+	game->mount = READ_BYTE(p);
+
+	game->options[5] = READ_BYTE(p);//CGA palette
+
+	/* Spells sold in towns */
+	for (n = 0; n < MAX_TOWNS; n++)
+		game->town_spell[n] = READ_BYTE(p);
+
+	/* Town/Contract */
+	for (n = 0; n < 5; n++)
+		game->contract_cycle[n] = READ_BYTE(p);
+	game->last_contract =  READ_BYTE(p);
+	game->max_contract = READ_BYTE(p);
+
+	/* Steps left till end of day */
+	game->steps_left = READ_BYTE(p);
+
+	/* Skip 1 byte */
+	game->unknown3 = READ_BYTE(p);
+
+	/* Castle ownership */
+	for (n = 0; n < MAX_CASTLES; n++)
+		game->castle_owner[n] = READ_BYTE(p);
+
+	/* Visited castles and towns */
+	for (n = 0; n < MAX_CASTLES; n++)
+		game->castle_visited[n] = READ_BYTE(p);
+
+	for (n = 0; n < MAX_TOWNS; n++)
+		game->town_visited[n] = READ_BYTE(p);
+
+	/* Scepter location (encrypted) */
+	game->scepter_continent = READ_BYTE(p);
+	game->scepter_x = READ_BYTE(p);
+	game->scepter_y = READ_BYTE(p);
+
+	/* Fog of war (convert from 1-bit-per-tile to 1-byte-per-tile format) */
+	map_size = ( MAX_CONTINENTS * LEVEL_W * LEVEL_H );
+	for (n = 0; n < MAX_CONTINENTS; n++) {
+		for (y = 0; y < LEVEL_H; y++)
+		for (x = 0; x < LEVEL_W; x += 8) {
+			char test_bits = READ_BYTE(p);
+			for (k = 0; k < 8; k++) {
+				char one_bit = ((test_bits & (0x01 << k)) >> k) & 0x01;
+				game->fog[n][y][x + 7 - k] = one_bit;
+			}
+		}
+	}
+
+	/* Waste AMIGA byte?! */
+	READ_BYTE(p);
+
+	/* Castle garrison troops */
+	for (n = 0; n < MAX_CASTLES; n++)
+		for (k = 0; k < 5; k++)
+			game->castle_troops[n][k] = READ_BYTE(p);
+
+	/* Read friendly foes' coords */
+	for (n = 0; n < MAX_CONTINENTS; n++) {
+		for (k = 0; k < FRIENDLY_FOES; k++) {
+			game->foe_coords[n][k][0] = READ_BYTE(p);//X
+			game->foe_coords[n][k][1] = READ_BYTE(p);//Y
+		}
+	}
+
+	/* Map chests */
+	for (n = 0; n < MAX_CONTINENTS - 1; n++) {
+		game->map_coords[n][0] = READ_BYTE(p);//X
+		game->map_coords[n][1] = READ_BYTE(p);//Y
+	}
+
+	/* Orb chests */
+	for (n = 0; n < MAX_CONTINENTS; n++) {
+		game->orb_coords[n][0] = READ_BYTE(p);//X
+		game->orb_coords[n][1] = READ_BYTE(p);//Y
+	}
+
+	/* Waste another AMIGA byte?! */
+	READ_BYTE(p);
+
+	/* Teleporting caves */
+	for (n = 0; n < MAX_CONTINENTS; n++) {
+		for (k = 0; k < MAX_TELECAVES; k++) {
+			game->teleport_coords[n][k][0] = READ_BYTE(p);//X
+			game->teleport_coords[n][k][1] = READ_BYTE(p);//Y
+		}
+	}
+
+	/* Dwellings locations */
+	for (n = 0; n < MAX_CONTINENTS; n++) {
+		for (k = 0; k < MAX_DWELLINGS; k++) {
+			game->dwelling_coords[n][k][0] = READ_BYTE(p);//X
+			game->dwelling_coords[n][k][1] = READ_BYTE(p);//Y
+		}
+	}
+
+	/* Read hostile foes' coords */
+	for (n = 0; n < MAX_CONTINENTS; n++) {
+		for (k = FRIENDLY_FOES; k < MAX_FOES; k++) {
+			game->foe_coords[n][k][0] = READ_BYTE(p);//X
+			game->foe_coords[n][k][1] = READ_BYTE(p);//Y
+		}
+	}
+
+	/* Read hostile foes' troops */
+	for (n = 0; n < MAX_CONTINENTS; n++) {
+		for (k = FRIENDLY_FOES; k < MAX_FOES; k++) {
+			game->foe_troops[n][k][0] = READ_BYTE(p);
+			game->foe_troops[n][k][1] = READ_BYTE(p);
+			game->foe_troops[n][k][2] = READ_BYTE(p);
+		}
+	}
+
+	/* Read foe numbers */
+	for (n = 0; n < MAX_CONTINENTS; n++) {
+		for (k = FRIENDLY_FOES; k < MAX_FOES; k++) {
+			game->foe_numbers[n][k][0] = READ_BYTE(p);
+			game->foe_numbers[n][k][1] = READ_BYTE(p);
+			game->foe_numbers[n][k][2] = READ_BYTE(p);
+		}
+	}
+
+	//READ_BYTE(p); //waste byte
+	//READ_BYTE(p); //waste byte
+
+	/* Read dwelling troop type and population count */
+	for (n = 0; n < MAX_CONTINENTS; n++)
+		for (k = 0; k < MAX_DWELLINGS; k++)
+			game->dwelling_troop[n][k] = READ_BYTE(p);
+
+	for (n = 0; n < MAX_CONTINENTS; n++)
+		for (k = 0; k < MAX_DWELLINGS; k++)
+			game->dwelling_population[n][k] = READ_BYTE(p);
+
+	/* Read scepter key and un-encrypt the coordinates */
+	//game->scepter_key = READ_BYTE(p);
+	//game->scepter_continent ^= game->scepter_key;
+	//game->scepter_x ^= game->scepter_key;
+	//game->scepter_y ^= game->scepter_key;
+
+	/* More stats */
+	game->base_leadership = READ_WORD_BE(p);
+	game->leadership = READ_WORD_BE(p);
+	game->commission = READ_WORD_BE(p);
+	game->followers_killed = READ_WORD_BE(p);
+
+	/* Player army numbers */
+	for (n = 0; n < 5; n++)
+		game->player_numbers[n] = READ_WORD_BE(p);
+
+	/* Castle garrison numbers */
+	for (n = 0; n < MAX_CASTLES; n++)
+		for (k = 0; k < 5; k++)
+			game->castle_numbers[n][k] = READ_WORD_BE(p);
+
+	/* More stats */
+	game->time_stop = READ_WORD_BE(p);
+	game->days_left = READ_WORD_BE(p);
+
+	/* Score (unused) */
+	game->score = READ_WORD_BE(p);
+
+	/* Skip 2 bytes */
+	game->unknown1 = READ_BYTE(p);
+	game->unknown2 = READ_BYTE(p);
+
+	/* Player's gold (32 bit, can get quite rich!) */
+	game->gold = READ_DWORD_BE(p);
+
+	/* Map dump */
+	n = map_size;
+	memcpy(game->map, p, n);
+	p += n;
+
+	if (p - &buf[0] != DAT_SIZE)
+	fprintf(stdout, "Save file: %d bytes (needed %d)\n", p - &buf[0], DAT_SIZE);
+
+	return game;
+}
+
 
 int KB_saveDAT(const char* filename, KBgame *game) {
 
